d7287937adb2fdf14ce6f43ae93e138b99de5b9d
Sunny Prakash <sunny.prakashgm@gmail.com>
2018-09-03 01:28:43 +0530

    first API in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  userName = "";  
+  fullName = "";
+  login = "";
+  avatar_url="";
+  searchFlag = false;
+  users:any;
+  constructor(private _http:HttpClient){  }
+
+  searchUser(){
+    this.searchFlag = true;
+    this._http.get("https://api.github.com/search/users?q="+this.userName+"+in%3Afullname&type=Users")
+                      .subscribe(data => {
+                        this.users = data;
+                        for(let i=0;i<this.users.items.length;i++){
+                            this._http.get("https://api.github.com/users/"+this.users.items[i].login)  
+                                .subscribe((res:Response)=>{
+                                this.fullName = res.name;
+                                console.log(this.fullName);
+                                });                           
+                        }
+                        
+                              
+                      });
+    //console.log(this.users.json());
+    // this._http.get("https://api.github.com/search/users?q="+this.userName+"+in%3Afullname&type=Users")
+    //             .subscribe(
+    //               (res:Response)=>{
+    //                 this.login = res.items[0].login;
+    //                 // this.avatar_url = res.items[0].avatar_url;
+    //                 this._http.get("https://api.github.com/users/"+this.login)  
+    //                     .subscribe((res:Response)=>{
+    //                     this.fullName = res.name;
+    //                     }); 
+    //               });
+
+     
+  } 
+  
 }
---
+++ src/app/user.ts
@@ -0,0 +1,5 @@
+export interface User{
+    total_count:number;
+    incomplete_results:boolean;
+    items:Object[];
+}
